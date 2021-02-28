ffd7374e941b652fbd3ee88d5957687aedce8b09
Oğuzhan D <oguzhandurgun95@gmail.com>
2021-02-28 19:28:21 +0300

    Structure and logging (#2)
    * Created project structure and added google logger
    
    * Changed structure

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,25 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"github.com/google/logger"
-	"os"
-)
-
-const (
-	logFileName = "go-web-crawler-starter.log"
+	"github.com/golang/glog"
+	"github.com/oguzhand95/go-web-crawler-starter/src/internal/configuration"
 )
 
 func main() {
-	defer logger.Init(logFileName, true, true, getLogFile()).Close()
-}
-
-func getLogFile() *os.File {
-	logFile, err := os.OpenFile(fmt.Sprintf("./%s", logFileName), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+	configuration, err := configuration.NewConfiguration()
 
 	if err != nil {
-		logger.Fatalf("Failed to open log file: %v", err)
+		glog.Fatalf("Failed to retrieve configuration:\n%s", err.Error())
 	}
 
-	return logFile
+	glog.Infof("controllers: %s", configuration.ControllerConfiguration.Controllers)
 }
