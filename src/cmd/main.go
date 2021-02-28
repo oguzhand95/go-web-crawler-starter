5163c9b6a6bf8da3380763f1a5eefcb15c812c79
Oğuzhan D <oguzhandurgun95@gmail.com>
2021-02-28 21:10:54 +0300

    Added context and WaitGroups (#4)

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"github.com/golang/glog"
 	controllerInterface "github.com/oguzhand95/go-web-crawler-starter/src/controller"
 	"github.com/oguzhand95/go-web-crawler-starter/src/controller/pubg.op.gg"
 	"github.com/oguzhand95/go-web-crawler-starter/src/internal/configuration"
 	"strings"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -29,11 +32,16 @@ func main() {
 
 	listOfControllers := getControllerListFromConfiguration(appConfiguration.ControllerConfiguration)
 
-	err = runControllers(controllerMap, listOfControllers)
+	ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+	waitGroup := &sync.WaitGroup{}
+
+	err = runControllers(ctx, waitGroup, controllerMap, listOfControllers)
 
 	if err != nil {
 		glog.Fatalf("failed during execution of controllers:%s\n", err.Error())
 	}
+
+	waitGroup.Wait()
 }
 
 func registerControllers(controllerMap map[string]controllerInterface.Controller) {
@@ -42,11 +50,18 @@ func registerControllers(controllerMap map[string]controllerInterface.Controller
 	controllerMap[pubgOpGgController.GetName()] = pubgOpGgController
 }
 
-func runControllers(controllerMap map[string]controllerInterface.Controller, controllersToRun []string) error {
+func runControllers(ctx context.Context, waitGroup *sync.WaitGroup,
+	controllerMap map[string]controllerInterface.Controller, controllersToRun []string) error {
 	for _, controllerName := range controllersToRun {
 		if val, ok := controllerMap[controllerName]; ok {
 			glog.Infof("running controller %q", val.GetName())
-			go val.Run()
+			waitGroup.Add(1)
+
+			go func(ctx context.Context, waitGroup *sync.WaitGroup) {
+				val.Run()
+				waitGroup.Done()
+				<-ctx.Done()
+			}(ctx, waitGroup)
 		} else {
 			glog.Warningf("controller %q could not found", controllerName)
 		}
